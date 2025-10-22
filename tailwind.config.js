0c59bc1b3cb02047d4d87173ab32832d585ea1d1
Much. Rifqi Hermawan <mrifqi939@gmail.com>
2025-10-22 21:50:38 +0700

    Personal web - start

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html", "./src/**/*.{js,html}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "ui-sans-serif", "system-ui"],
+      },
+      keyframes: {
+        "fade-in": {
+          "0%": { opacity: "0", transform: "translateY(1rem)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
+        },
+      },
+      animation: {
+        "fade-in": "fade-in 0.7s ease-out forwards",
+      },
+    },
+  },
+  plugins: [],
+};
